48b979b420d443bb82c7313778433b45ffc3e1f7
Prachi Pawar <prachipawar2705@gmail.com>
2025-07-19 00:22:50 +0530

    data update

--- src/sections/Profile.js
+++ src/sections/Profile.js
@@ -11,7 +11,7 @@ const Profile = () => {
                         <h3 className="profile-name">Hello! I’m Prachi Minal Pawar</h3>
                         <Typewriter
                             words={[
-                                "A pre-final year Computer Science and Technology student at Usha Mittal Institute of Technology (UMIT), currently holding an average CGPA of 9.008. I’m deeply passionate about innovation, problem-solving, and building meaningful solutions that blend creativity with technology.",
+                                "A final year Computer Science and Technology student at Usha Mittal Institute of Technology (UMIT), currently holding an average GPA of 8.9967. I’m deeply passionate about innovation, problem-solving, and building meaningful solutions that blend creativity with technology.",
                                 "My journey spans across diverse domains — from Web and Android development to Cybersecurity and the evolving world of Generative AI. I’m familiar wiith technologies like HTML, CSS, ReactJS, Java, etc. and I enjoy complementing my technical work with clean, thoughtful design using tools like Figma and Canva.",
                                 "I’m equally driven by curiosity — from exploring Robotics, Cloud Computing, Space Research, and Military Application to expressing myself through Art, Sports, Photography, and Creative Writing. With a strong foundation, a growth mindset, and a love for learning, I strive to embrace new challenges and create solutions that leave a lasting impact."
                             ]}
