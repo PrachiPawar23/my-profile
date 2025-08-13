07ba6e9ba82a88e076a933a4d7b308a2cd36010c
Prachi Pawar <prachipawar2705@gmail.com>
2025-08-13 20:40:18 +0530

    data update

--- src/sections/Interest.js
+++ src/sections/Interest.js
@@ -3,7 +3,7 @@ import "./Interest.css";
 
 const Interest = () => {
     const [interests] = useState([
-        "Cybersecurity", "Web Development", "Software Development", "Cloud Computing",
+        "Cybersecurity", "Web Development", "Software Development",
         "Mobile App Development", "Android Development", "Game Development",
         "Artificial Intelligence", "Machine Learning", "Neural Networks", "Deep Learning",
         "User Interface (UI) Design", "User Experience (UX) Design", "Robotics",
