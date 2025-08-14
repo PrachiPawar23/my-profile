fc6e4ee190930c42754877aad2b00ef1d9eb141f
Prachi Pawar <prachipawar2705@gmail.com>
2025-08-14 21:52:31 +0530

    data content fix

--- src/sections/Profile.js
+++ src/sections/Profile.js
@@ -12,7 +12,7 @@ const Profile = () => {
                         <Typewriter
                             words={[
                                 "A final year Computer Science and Technology student at Usha Mittal Institute of Technology (UMIT), currently holding an average GPA of 8.9967. I’m deeply passionate about innovation, problem-solving, and building meaningful solutions that blend creativity with technology.",
-                                "My journey spans across diverse domains — from Web and Android development to Cybersecurity and the evolving world of Generative AI. I’m familiar wiith technologies like HTML, CSS, ReactJS, Java, etc. and I enjoy complementing my technical work with clean, thoughtful design using tools like Figma and Canva.",
+                                "My journey spans across diverse domains — from Web and Android development to Cybersecurity and the evolving world of Generative AI. I’m familiar with technologies like HTML, CSS, ReactJS, Java, etc. and I enjoy complementing my technical work with clean, thoughtful design using tools like Figma and Canva.",
                                 "I’m equally driven by curiosity — from exploring Robotics, Cloud Computing, Space Research, and Military Application to expressing myself through Art, Sports, Photography, and Creative Writing. With a strong foundation, a growth mindset, and a love for learning, I strive to embrace new challenges and create solutions that leave a lasting impact."
                             ]}
                             typeSpeed={30}
